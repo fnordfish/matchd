907c448217af2f8801e29066ecb40a4be414ee1a
Robert Schulze <robert@dotless.de>
2018-08-14 12:58:39 +0200

    fallback passthrough will use the “force” flag
    + internal renaming “registry” -> “rules”

--- lib/matchd/server.rb
+++ lib/matchd/server.rb
@@ -1,24 +1,31 @@
 require "async/dns"
 
 module Matchd
+  # Specific implementation of the {Async::DNS::Server} using a list of rules to determine it's response
+  #
+  # Rules will get {#call}'ed in order until the first one matches (returns truthy).
+  # If none matches, a default {Matchd::Rule::Passthrough} will get executed forwarding the query to the resolver
+  # defined via the `:resolver` option.
   class Server < Async::DNS::Server
-    # @param registry [Matchd::Registry] A rules registry
+    # @param rules [Matchd::Registry|Array<Matchd.Rule>] A rules registry
     # @param listen [Array] On which interfaces to listen (`[[<protocol>, <ip>, <port>], ...]`). See `Matchd::Glue::AsyncEndpoint` for more formats.
-    # @param [Hash] opts
-    # @option opts [Array|Async::DNS::Resolver] :resolver The upstream resolvers (`[[<protocol>, <ip>, <port>], ...]`). See `Matchd::Glue::AsyncEndpoint` for more formats. (default: `Async::DNS::System.nameservers`)
-    def initialize(registry, listen, options = {})
-      @registry = registry
+    # @param [Hash] options
+    # @option options [Array|Async::DNS::Resolver] :resolver The upstream resolvers (`[[<protocol>, <ip>, <port>], ...]`). See `Matchd::Glue::AsyncEndpoint` for more formats. (default: `Async::DNS::System.nameservers`)
+    # @option options [TrueClass|FalseClass] :forced_passthtough The default Passthrough rule's `force` option
+    def initialize(rules, listen, options = {})
+      @rules = rules
       @resolver = options.delete(:resolver)
+      @forced_passthtough = options.delete(:forced_passthtough) { true }
 
       listen = Matchd::Glue::AsyncEndpoint.parse(listen)
 
-      super(listen, options)
+      super(listen, *options)
     end
 
-    attr_reader :registry, :resolver
+    attr_reader :rules, :resolver
 
     def process(name, resource_class, transaction)
-      found = registry.any? do |rule|
+      found = rules.any? do |rule|
         rule.call(self, name, resource_class, transaction)
       end
 
@@ -29,7 +36,8 @@ module Matchd
       Matchd::Rule::Passthrough.new(
         "passthrough" => resolver,
         "match" => name,
-        "resource_class" => resource_class
+        "resource_class" => resource_class,
+        "force" => @forced_passthtough
       ).visit!(self, name, resource_class, transaction)
     end
   end
